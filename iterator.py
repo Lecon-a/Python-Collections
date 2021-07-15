d91c1db6515a4295592bbb2f5ece19cb519fb712
Sunday P. Afolabi <31808402+Lecon-Git@users.noreply.github.com>
2021-07-14 21:31:53 -0500

    Update iterator.py
    Update iterator.py file by implementing the iterator, square, and show functions.

---
+++ iterator.py
@@ -0,0 +1,34 @@
+### This function takes in an arg... upon which numbers are yielded
+### Here is a function which yield a range of numbers... This range takes into consideration
+### both onset and the offset.
+def my_range(x):
+    i = 1
+    while i <= x:
+        yield i
+        i += 1
+
+### Here is a square function which takes an arg, squares it and return its square.
+def square(n):
+    return n ** 2
+
+def show():
+    error = False
+    try:
+        x = int(input("Enter value for n range: ")) 
+    except:
+        print("Error: You've Entered Invalid Value. Try Again!")
+        error = True
+    finally:
+        #if response is failure return
+        if error:
+            return
+        #if response is success to the following
+        for i in my_range(x):
+            d = 10
+            #logic for multiple of 10
+            if i % d == 0:
+                output = str(i) + " is a multiple of (" + str(d) + ") and its square is " + str(square(i)) + ".\n"
+                with open('output.txt', 'a') as f:
+                    f.write(output)
+
+show()
